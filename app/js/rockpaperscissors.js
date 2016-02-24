968ade3777a377c7519142c79ac7b2f6aa8f7225
Brent Rusinow <brentrusinow@Brents-15-MBP-2.lan>
2016-02-24 13:36:16 -0800

    updated conditions on player and computer moves

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,15 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+   var move = x;
+   x || getinput();
+    return getComputerMove();
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+   var move = x;
+   x || randomPlay();
+    return getPlayerMove();
 }
 
 function getWinner(playerMove,computerMove) {
